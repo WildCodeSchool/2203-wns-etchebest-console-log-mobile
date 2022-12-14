1c29390b210e46616db7ad517109fa8742010500
lauraduboeuf <laura.duboeuf@qwenty.fr>
2022-12-14 10:42:22 +0100

    add AuthInput debut

---
+++ __tests__/AuthInput.test.tsx
@@ -0,0 +1,10 @@
+/* import * as React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react-native';
+import AuthInput from '../components/AuthInput'
+;
+describe('AuthInput', () => {
+  test('render component authInput', () => {
+    render(<AuthInput index={0} control={undefined} placeholder={''} name={''} />);
+  });
+});
+ */
