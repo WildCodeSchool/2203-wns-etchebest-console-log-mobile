c611016d01d7de9ac26f1c22fded209deb75cfd2
Quentin Rault <q.rault054@gmail.com>
2022-12-22 13:23:02 +0100

    incorporate modifications

--- screens/ProjectsScreen.tsx
+++ screens/ProjectsScreen.tsx
@@ -16,13 +16,38 @@ const ProjectsScreen: React.FC = () => {
   const { data } = useQuery(allProjectsQueryDocument);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      {/* <View
+        style={{
+          flex: 0.2,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Autocomplete
+          style={styles.autoComplete}
+          placeholder=" Enter the project name"
+          data={filteredProjects}
+          defaultValue={JSON.stringify(selectedValue) === '{}' ? '' : ''}
+          inputContainerStyle={{ borderRadius: 10 }}
+        ></Autocomplete>
+      </View> */}
       <FlatList data={data?.projects ?? []} renderItem={ProjectCard} />
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  autoComplete: {
+    backgroundColor: COLORS.white,
+    borderRadius: 10,
+    borderWidth: 0,
+    height: 30,
+    justifyContent: 'center',
+    position: 'relative',
+    width: 300,
+  },
   container: {
     alignItems: 'center',
     backgroundColor: COLORS.primary,
